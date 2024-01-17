5e491a1a5b77ce8231a7a4fb317396e39fe3aa1f
katebry <katebryan31@gmail.com>
2024-01-17 11:01:41 +0000

    package json tweaks to use nodemon

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import express, {Express} from "express";
+import { createHandler } from 'graphql-http/lib/use/http';
 
 const app: Express = express();
+app.use('/graphql', createHandler({}))
 
 app.listen(4000, (): void => {
     console.log("Listening...")
